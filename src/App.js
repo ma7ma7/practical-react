2dcd5868b1f766a109ee6e9c1f24deaea9aa4075
Mahmoud Amane <mahmoudamane5@gmail.com>
2021-04-28 16:10:32 +0000

    Setup React-toastify

--- src/App.js
+++ src/App.js
@@ -1,7 +1,45 @@
 import "./App.css";
 
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 function App() {
-  return <div className="App"></div>;
+  const notify = () => {
+    toast("Default Notification 🚀");
+
+    // Success
+    toast.success("Success Notification ☑️", {
+      position: toast.POSITION.TOP_CENTER,
+      autoClose: 2000,
+    });
+
+    // Error
+    toast.error("Error Notification ☀️", {
+      position: toast.POSITION.TOP_LEFT,
+    });
+
+    // Warn
+    toast.warn("Warnning Notification 🚨", {
+      position: toast.POSITION.BOTTOM_LEFT,
+    });
+
+    // Warn
+    toast.info("Info Notification 🎄", {
+      position: toast.POSITION.BOTTOM_RIGHT,
+    });
+
+    toast("custom style", {
+      position: toast.POSITION.BOTTOM_CENTER,
+      className: "toast",
+      autoClose: false,
+    });
+  };
+  return (
+    <div className="App">
+      <button onClick={notify}>Toast</button>
+      <ToastContainer />
+    </div>
+  );
 }
 
 export default App;
