c5315840ab03ec20d6d63c5f20c60503e3da4e47
Mahmoud Amane <mahmoudamane5@gmail.com>
2021-05-03 00:15:44 +0000

    React Modal

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
 import "./App.css";
+import Modal from "react-modal";
+import { useState } from "react";
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
+
+Modal.setAppElement("#root");
 
 function App() {
-  return <div className="App"></div>;
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  return (
+    <div className="App">
+      <button onClick={() => setModalIsOpen(true)}>Open modal</button>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={() => setModalIsOpen(false)}
+        style={customStyles}
+      >
+        <h2>Hello</h2>
+        <div>I am a modal</div>
+        <button onClick={() => setModalIsOpen(false)}>Close</button>
+        <form>
+          <input />
+          <button>tab navigation</button>
+          <button>stays</button>
+          <button>inside</button>
+          <button>the modal</button>
+        </form>
+      </Modal>
+    </div>
+  );
 }
 
 export default App;
